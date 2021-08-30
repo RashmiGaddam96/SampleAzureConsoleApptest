97abf59cc3fa8c6e6535b3618c3b27a0703d62ec
RashmiGaddam <rashmi.gaddam96@gmail.com>
2021-08-30 16:45:43 +0530

    Sample

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SampleAzureConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Azure!");
+            Console.WriteLine("Hello Azure!");
+            Console.ReadLine();
+        }
+    }
+}
